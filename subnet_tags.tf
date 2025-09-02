4ddec55d0180e0394a9ea08419d7d0e724ebf85d
fidiego <sayheytofred@gmail.com>
2025-09-01 23:04:46 -0500

    chore: always tag the discovered private subnets with the cluster name
    in the past, we were only doing this if the cluster name was not equal
    to the install_id. we've removed the pre-emptive tagging from the
    cloudformation code[1] and must therefore ensure this tag is always
    applied.
    
    Note: it seems we were NOT using the same condition for th epublic
    subnets. this was not intended.
    
    [1] https://github.com/nuonco/aws-cloudformation-templates/pull/12

--- subnet_tags.tf
+++ subnet_tags.tf
@@ -1,7 +1,4 @@
 module "additional_subnet_tags" {
-  # only create if the cluster name does not equal the nuon id
-  count = module.eks.cluster_name == var.nuon_id ? 0 : 1
-
   source = "./subnet_tags"
 
   eks_cluster_name   = module.eks.cluster_name
